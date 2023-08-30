4f33aae91a57433ea3705b1a9833b7de9e80bfe4
Max Chen <max.chen2@vpon.com>
2023-08-30 10:47:52 +0800

    feature(i18n) change zh into zh-tw

--- i18n/lang/common.zh.ts
+++ i18n/lang/common.zh.ts
@@ -2,20 +2,20 @@ const translation = {
   api: {
     success: '成功',
     saved: '已保存',
-    create: '已创建',
+    create: '已建立',
   },
   operation: {
-    confirm: '确认',
+    confirm: '確認',
     cancel: '取消',
     clear: '清空',
     save: '保存',
-    edit: '编辑',
-    refresh: '重新开始',
+    edit: '編輯',
+    refresh: '重新開始',
     search: '搜索',
-    send: '发送',
-    lineBreak: '换行',
-    like: '赞同',
-    dislike: '反对',
+    send: '發送',
+    lineBreak: '換行',
+    like: '贊同',
+    dislike: '反對',
   },
 }
 
